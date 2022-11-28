0a5c83600ffa81b17a0d200b018c3908b5a3e740
holleywood6390 <90008436+holleywood6390@users.noreply.github.com>
2022-11-28 12:27:41 -0500

    2 files modified

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -12,6 +12,29 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('We have a user logged in: ' + user.email);
+  } else {
+    console.log('No user logged in');
+    window.location.href = 'index.html';
+  }
+});
+
+$('#signout').click(function () {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Signout successful
+      window.location.href = 'index.html';
+    })
+    .catch((error) => {
+      // An error occured
+      console.log(error.message);
+    });
+});
+
 // save the data
 $(".sampleSurvey input[type='submit']").click(function (e) {
   e.preventDefault();
@@ -37,30 +60,35 @@ firebase
   .firestore()
   .collection('surveydata')
   .onSnapshot((querySnapshot) => {
-    var n1=0; // How many A
-    var n2=0; // How many B
-    var n3=0; // How many C
-    var n4=0; // How many D
-    var n5=0; // How many E
+    var n1 = 0; // How many A
+    var n2 = 0; // How many B
+    var n3 = 0; // How many C
+    var n4 = 0; // How many D
+    var n5 = 0; // How many E
     console.log(querySnapshot.size);
     querySnapshot.forEach((doc) => {
       console.log(doc.data().choice);
       var s = doc.data().choice;
-      switch(s){
-        case "A":
-          n1++; $('#ans1').text(n1);
+      switch (s) {
+        case 'A':
+          n1++;
+          $('#ans1').text(n1);
           break;
-        case "B":
-          n2++; $('#ans2').text(n2);
+        case 'B':
+          n2++;
+          $('#ans2').text(n2);
           break;
-        case "C":
-          n3++; $('#ans3').text(n3);
+        case 'C':
+          n3++;
+          $('#ans3').text(n3);
           break;
-        case "D":
-          n4++; $('#ans4').text(n4);
+        case 'D':
+          n4++;
+          $('#ans4').text(n4);
           break;
-        case "E":
-          n5++; $('#ans5').text(n5);
+        case 'E':
+          n5++;
+          $('#ans5').text(n5);
           break;
       }
       console.log(doc.data().comm);
